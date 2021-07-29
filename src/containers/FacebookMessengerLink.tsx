0d20f354f1547a905b8ebf523ce6d07abcb67f6d
Doan Minh <minh.doan@vmodev.com>
2021-07-29 20:54:45 +0700

    Update

--- src/containers/FacebookMessengerLink.tsx
+++ src/containers/FacebookMessengerLink.tsx
@@ -1,8 +1,10 @@
 import React, { FC, useState, useCallback } from "react";
+import { v4 } from "uuid";
 import ReactMessengerCustomerChat from "react-messenger-customer-chat";
 
 const FacebookMessengerLink: FC = () => {
   const [show, setShow] = useState<boolean>(false);
+
   const handleFocusIframe = useCallback(() => {
     setShow(true);
   }, []);
@@ -11,11 +13,12 @@ const FacebookMessengerLink: FC = () => {
     <div className="Facebook-messenger">
       <button onClick={handleFocusIframe}>Focus</button>
       <ReactMessengerCustomerChat
+        key={v4()}
         pageId="771400903577002"
         appId="810978546253814"
         shouldShowDialog={show}
         onCustomerChatDialogShow={() => setShow(true)}
-        onCustomerChatDialogHide={() => setShow(true)}
+        onCustomerChatDialogHide={() => setShow(false)}
       />
     </div>
   );
