76388eec9d2fdfcbfd895dc6158b3bb985e92ae6
Doan Minh <minh.doan@vmodev.com>
2021-07-29 21:31:40 +0700

    Update

--- src/containers/FacebookMessengerLink.tsx
+++ src/containers/FacebookMessengerLink.tsx
@@ -1,12 +1,7 @@
-import React, { FC, useState, useCallback } from "react";
-import { v4 } from "uuid";
+import React, { FC, useCallback } from "react";
 import ReactMessengerCustomerChat from "react-messenger-customer-chat";
 
-const initial = v4();
-
 const FacebookMessengerLink: FC = () => {
-  const [key, setKey] = useState<string>(initial);
-
   const handleFocusIframe = useCallback(() => {
     (window as any).FB.CustomerChat.showDialog();
   }, []);
